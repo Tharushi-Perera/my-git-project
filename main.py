45ad55630297defb5602f05a81abeab21aaf8e7e
MiTilk <129934614+MihanTilk@users.noreply.github.com>
2025-03-25 16:30:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from Basic operations import add, subtract, multiply
-from Greetingss import greet_user
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizInterface
 
-print(greet_user("Tharushi"))
-print("Addition:", add(10, 5))
-print("Subtraction:", subtract(10, 5))
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
